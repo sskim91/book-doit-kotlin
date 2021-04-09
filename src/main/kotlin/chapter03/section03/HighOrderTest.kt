2c1645e6e25b300568df59c5623dbd95a43a89ac
sskim <tjdtjq91@gmail.com>
2021-04-09 14:55:36 +0900

    Fix: 함수 예제 수정

--- src/main/kotlin/chapter03/section03/HighOrderTest.kt
+++ src/main/kotlin/chapter03/section03/HighOrderTest.kt
@@ -6,7 +6,12 @@ package chapter03.section03
 //변수에 할당하는 람다식 함수 작성하기
 fun main() {
     var result: Int
-    val multi = { x: Int, y: Int -> x * y }    //일반 변수에 람다식 활용
+    val multi: (Int, Int) -> Int = { x: Int, y: Int -> x * y }    //일반 변수에 람다식 활용
+//
+//    val multi: (Int, Int) -> Int = { x: Int, y: Int -> x * y }  //전체 표현
+//    val multi = { x: Int, y: Int -> x * y } //선언 자료형 생략
+//    val multi: (Int, Int) -> Int = { x, y -> x * y }    //람다식 매개변수 자료형 생략
+
     result = multi(10, 20)   //람다식이 할당된 변수는 함수처럼 사용 가능
     println(result)
 }
